197fe851f7776871ac5ae8557d36bfa72a0d422b
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-03-11 13:16:57 -0300

    feat: Multiple ping on same conn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::from_utf8};
 
-use tokio::{io::AsyncWriteExt, net::TcpListener};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
-const PING_RESPONDE: [u8; 7] = *b"+PONG\r\n";
+const PING_RESPONSE: [u8; 7] = *b"+PONG\r\n";
+
+enum RedisCommands {
+    Ping,
+}
+
+impl ToString for RedisCommands {
+    fn to_string(&self) -> String {
+        match self {
+            RedisCommands::Ping => "ping\r\n".to_owned(),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,12 +27,25 @@ async fn main() {
     let listener = TcpListener::bind(socket_addr).await.unwrap();
     loop {
         match listener.accept().await {
-            Ok((mut stream, _)) => {
-                stream.write_all(&PING_RESPONDE).await.unwrap();
-            }
+            Ok((mut stream, _)) => loop {
+                handle_client(&mut stream).await;
+            },
             Err(e) => {
                 eprintln!("Error: {}", e);
             }
         }
+    }
+}
+
+async fn handle_client(stream: &mut TcpStream) {
+    let mut buf = [0; 1024];
+    stream.read(&mut buf).await.unwrap();
+    let cmd = from_utf8(&buf).unwrap();
+    println!("{}", cmd);
+    match cmd {
+        c if c.contains(&RedisCommands::Ping.to_string()) => {
+            stream.write_all(&PING_RESPONSE).await.unwrap();
+        }
+        _ => unimplemented!(),
     }
 }
