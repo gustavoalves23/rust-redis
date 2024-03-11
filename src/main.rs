4043a93a2add2b59cda4da7f45f735a9b53a898f
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-03-11 12:45:30 -0300

    feat: Respond to PING task

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::net::SocketAddr;
 
-use tokio::net::TcpListener;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
+
+const PING_RESPONDE: [u8; 7] = *b"+PONG\r\n";
 
 #[tokio::main]
 async fn main() {
@@ -10,8 +12,8 @@ async fn main() {
     let listener = TcpListener::bind(socket_addr).await.unwrap();
     loop {
         match listener.accept().await {
-            Ok((stream, _)) => {
-                println!("Accepted new connection!, Stream: {:?}", stream);
+            Ok((mut stream, _)) => {
+                stream.write_all(&PING_RESPONDE).await.unwrap();
             }
             Err(e) => {
                 eprintln!("Error: {}", e);
