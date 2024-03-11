2e691a238576edc1cb9b68b5d584d615cbfcf07c
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-03-11 17:22:38 -0300

    feat: Add echo command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::SocketAddr, str::from_utf8};
+use std::{convert::Infallible, fmt::Error, net::SocketAddr, str::from_utf8};
 
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -7,14 +7,17 @@ use tokio::{
 
 const PING_RESPONSE: [u8; 7] = *b"+PONG\r\n";
 
+#[derive(Debug)]
 enum RedisCommands {
     Ping,
+    Echo,
 }
 
 impl RedisCommands {
-    fn as_command(&self) -> String {
+    fn as_command(&self) -> &str {
         match self {
-            RedisCommands::Ping => "ping\r\n".to_owned(),
+            RedisCommands::Ping => "PING",
+            RedisCommands::Echo => "ECHO",
         }
     }
 }
@@ -27,24 +30,72 @@ async fn main() {
     let listener = TcpListener::bind(socket_addr).await.unwrap();
 
     loop {
-        if let Ok((stream, _)) = listener.accept().await {
-            tokio::spawn(handle_client(stream));
+        if let Ok((mut stream, _)) = listener.accept().await {
+            tokio::spawn(async move {
+                loop {
+                    handle_client(&mut stream).await
+                }
+            });
         } else {
             panic!()
         }
     }
 }
 
-async fn handle_client(mut stream: TcpStream) {
-    loop {
-        let mut buf = [0; 1024];
-        stream.read(&mut buf).await.unwrap();
-        let cmd = from_utf8(&buf).unwrap();
-        match cmd {
-            c if c.contains(&RedisCommands::Ping.as_command()) => {
-                stream.write_all(&PING_RESPONSE).await.unwrap();
+fn parse_redis_command<'a>(
+    raw_command: &'a str,
+) -> Result<(RedisCommands, Vec<&'a str>, i32), Error> {
+    match raw_command.strip_prefix("*") {
+        Some(cmd) => {
+            let mut command_arr = cmd.split("\r\n").into_iter();
+            let count: i32 = command_arr.next().unwrap().parse::<i32>().unwrap();
+            command_arr.next();
+
+            let command = match command_arr.next().unwrap().to_uppercase().trim() {
+                cmd if cmd == RedisCommands::Ping.as_command() => RedisCommands::Ping,
+                cmd if cmd == RedisCommands::Echo.as_command() => RedisCommands::Echo,
+                _cmd => panic!(),
+            };
+            let args: Vec<_> = command_arr
+                .filter(|x| x != &"" && !x.contains("$"))
+                .collect();
+
+            Ok::<_, _>((command, args, count))
+        }
+        None => Err(Error),
+    }
+}
+
+async fn handle_ping_command(stream: &mut TcpStream) -> Result<(), Infallible> {
+    stream.write_all(&PING_RESPONSE).await.unwrap();
+    Ok(())
+}
+
+async fn handle_echo_command(stream: &mut TcpStream, args: Vec<&str>) -> Result<(), Error> {
+    if args.len() != 1 {
+        return Err(Error);
+    }
+
+    let msg = args.iter().next().unwrap();
+    let msg_len = msg.len();
+    let res = format!("${msg_len}\r\n{msg}\r\n");
+
+    stream.write_all(res.as_bytes()).await.unwrap();
+    Ok(())
+}
+
+async fn handle_client(stream: &mut TcpStream) {
+    let mut buf = [0; 1024];
+    let range = stream.read(&mut buf).await.unwrap();
+    let cmd = from_utf8(&buf[0..range]).unwrap();
+    if let Ok(data) = parse_redis_command(cmd) {
+        match data {
+            (RedisCommands::Ping, _, _) => {
+                handle_ping_command(stream).await.unwrap();
             }
-            _ => unimplemented!(),
+            (RedisCommands::Echo, args, _) => {
+                handle_echo_command(stream, args).await.unwrap();
+            }
         }
     }
 }
