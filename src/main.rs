951f9e0e040c868a2d2e41e8a20de385eabe47b9
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-03-11 10:20:52 -0300

    refactor: Alter main function to be async with the tokio rt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use std::net::TcpListener;
+use tokio::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+    match listener.accept().await {
+        Ok(_socket) => {
+            println!("Accepted new connection!");
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
         }
     }
 }
