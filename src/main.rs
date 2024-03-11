e07e7eadcb14e68307152acfb47f7985b854ddd5
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-03-11 18:10:49 -0300

    feat: Commands to Set and Get strings

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use std::{convert::Infallible, fmt::Error, net::SocketAddr, str::from_utf8};
+use std::{
+    collections::HashMap,
+    convert::Infallible,
+    fmt::Error,
+    net::SocketAddr,
+    str::from_utf8,
+    sync::{Arc, Mutex},
+};
 
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-const PING_RESPONSE: [u8; 7] = *b"+PONG\r\n";
-
 #[derive(Debug)]
 enum RedisCommands {
     Ping,
     Echo,
+    Set,
+    Get,
 }
 
 impl RedisCommands {
@@ -18,6 +25,8 @@ impl RedisCommands {
         match self {
             RedisCommands::Ping => "PING",
             RedisCommands::Echo => "ECHO",
+            RedisCommands::Set => "SET",
+            RedisCommands::Get => "GET",
         }
     }
 }
@@ -29,11 +38,15 @@ async fn main() {
 
     let listener = TcpListener::bind(socket_addr).await.unwrap();
 
+    let storage: Arc<Mutex<HashMap<String, String>>> =
+        Arc::new(Mutex::new(HashMap::<String, String>::new()));
     loop {
         if let Ok((mut stream, _)) = listener.accept().await {
+            let storage = Arc::clone(&storage);
             tokio::spawn(async move {
                 loop {
-                    handle_client(&mut stream).await
+                    let storage = Arc::clone(&storage);
+                    handle_client(&mut stream, storage).await
                 }
             });
         } else {
@@ -54,6 +67,8 @@ fn parse_redis_command<'a>(
             let command = match command_arr.next().unwrap().to_uppercase().trim() {
                 cmd if cmd == RedisCommands::Ping.as_command() => RedisCommands::Ping,
                 cmd if cmd == RedisCommands::Echo.as_command() => RedisCommands::Echo,
+                cmd if cmd == RedisCommands::Set.as_command() => RedisCommands::Set,
+                cmd if cmd == RedisCommands::Get.as_command() => RedisCommands::Get,
                 _cmd => panic!(),
             };
             let args: Vec<_> = command_arr
@@ -66,7 +81,18 @@ fn parse_redis_command<'a>(
     }
 }
 
+fn write_to_storage(storage: Arc<Mutex<HashMap<String, String>>>, val: (String, String)) {
+    let mut storage = storage.lock().unwrap();
+    let (k, v) = val;
+    storage.insert(k, v);
+}
+fn read_from_storage(storage: Arc<Mutex<HashMap<String, String>>>, key: String) -> String {
+    let storage = storage.lock().unwrap();
+    storage.get(&key).unwrap().to_string()
+}
+
 async fn handle_ping_command(stream: &mut TcpStream) -> Result<(), Infallible> {
+    const PING_RESPONSE: [u8; 7] = *b"+PONG\r\n";
     stream.write_all(&PING_RESPONSE).await.unwrap();
     Ok(())
 }
@@ -84,7 +110,40 @@ async fn handle_echo_command(stream: &mut TcpStream, args: Vec<&str>) -> Result<
     Ok(())
 }
 
-async fn handle_client(stream: &mut TcpStream) {
+async fn handle_set_command(
+    stream: &mut TcpStream,
+    args: Vec<&str>,
+    storage: Arc<Mutex<HashMap<String, String>>>,
+) -> Result<(), Infallible> {
+    let mut args = args.iter();
+    let key = args.next().unwrap();
+    let val = args.next().unwrap();
+
+    write_to_storage(storage, (key.to_string(), val.to_string()));
+
+    stream.write_all(b"+OK\r\n").await.unwrap();
+    Ok(())
+}
+
+async fn handle_get_command(
+    stream: &mut TcpStream,
+    args: Vec<&str>,
+    storage: Arc<Mutex<HashMap<String, String>>>,
+) -> Result<(), Error> {
+    if args.len() != 1 {
+        return Err(Error);
+    }
+
+    let key = args.iter().next().unwrap();
+    let msg = read_from_storage(storage, key.to_string());
+    let msg_len = msg.len();
+    let res = format!("${msg_len}\r\n{msg}\r\n");
+
+    stream.write_all(res.as_bytes()).await.unwrap();
+    Ok(())
+}
+
+async fn handle_client(stream: &mut TcpStream, storage: Arc<Mutex<HashMap<String, String>>>) {
     let mut buf = [0; 1024];
     let range = stream.read(&mut buf).await.unwrap();
     let cmd = from_utf8(&buf[0..range]).unwrap();
@@ -95,6 +154,12 @@ async fn handle_client(stream: &mut TcpStream) {
             }
             (RedisCommands::Echo, args, _) => {
                 handle_echo_command(stream, args).await.unwrap();
+            }
+            (RedisCommands::Set, args, _) => {
+                handle_set_command(stream, args, storage).await.unwrap();
+            }
+            (RedisCommands::Get, args, _) => {
+                handle_get_command(stream, args, storage).await.unwrap();
             }
         }
     }
